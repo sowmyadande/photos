c7f72630f8b5135b5cbfd5b9a449924d46120683
sowmyadande <saisowmyadande@gmail.com>
2022-09-08 12:21:59 +0530

    committed

---
+++ prog.java
@@ -0,0 +1,29 @@
+package variablepractice;
+
+public class variable {
+	static int b=3; //3
+	final int c=4;//4
+	  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(c<=4)//T
+		{
+			c=3;
+			int d=b+c;
+			System.out.println(d);
+		}
+		int d=b+c;
+		System.out.println(d);//7
+	
+	int f=3;
+	int g=4;
+	       if(g<3)
+	{
+	    	   g=2;
+		}
+	       int h=f+g;
+	       System.out.println(h);
+
+
+	}
+	}
